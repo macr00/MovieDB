be736ac2a0ca188dfb6c7d57f7cd7dc762b42382
Rory McCormack <mccormack.rory@gmail.com>
2018-01-10 12:57:17 +1100

    Added detail fragment to module

--- app/src/main/java/com/moviedb/ui/common/FragmentsModule.kt
+++ app/src/main/java/com/moviedb/ui/common/FragmentsModule.kt
@@ -1,5 +1,6 @@
 package com.moviedb.ui.common
 
+import com.moviedb.ui.details.MovieDetailsFragment
 import com.moviedb.ui.list.MovieListFragment
 import com.moviedb.ui.list.MovieListModule
 import com.moviedb.ui.search.SearchFragment
@@ -15,4 +16,7 @@ abstract class FragmentsModule {
 
     @ContributesAndroidInjector(modules = arrayOf(SearchModule::class))
     abstract fun searchFragment(): SearchFragment
+
+    @ContributesAndroidInjector()
+    abstract fun movieDetailsFragment(): MovieDetailsFragment
 }
